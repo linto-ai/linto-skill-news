db0c5686200584e2affe3070416b49357a819e55
HOUPERT <yoann.houpert@yahoo.fr>
2020-10-22 15:00:04 +0200

    added description

--- linto-skill-news.js
+++ linto-skill-news.js
@@ -17,7 +17,8 @@ module.exports = function (RED) {
         lintoSkillNews: {
           value: {
             template: template.settupSkillTemplate(PALETTE_NODE_NAME),
-            command: LintoSkillNode.loadFile(__dirname, 'data/command.md')
+            command: LintoSkillNode.loadFile(__dirname, 'data/command.md'),
+            description: LintoSkillNode.loadFile(__dirname, 'data/description.json')
           },
           exportable: true
         }
